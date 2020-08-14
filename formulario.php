308915568873c230bc39db93668ac4d243c72d24
licetteG <69667192+licetteG@users.noreply.github.com>
2020-08-14 15:01:32 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title></title>
+	<link type="text/css" rel="stylesheet" href="static/css/estilo1.css">
+
+
+
+
+</head>
+
+<body>
+
+	<nav  class="MainMenu">
+
+
+			<ul class="MainMenu-list">
+
+			<marquee>
+				<?php
+//validacion del formulario
+
+$n = $_POST["nombre"];
+echo "<h1> Bienvenida cumpleañera ".$n." y que tengas un bonito día </h1>";
+
+
+?>
+				</marquee>	
+			</ul>
+		</nav>
+
+
+	<marquee>
+	
+	
+
+	</marquee>
+
+
+
+<figure class="Header-imageContainer">
+			<img class="Header-image" src="images/2020.jpg" alt="Imagen Personal" width="20%" height="30%">
+		</figure>
+
+<video  src="video/globos3.mp4" controls autoplay loop ></video>
+
+
+</body>
+</html>
+
+
+
+ 
